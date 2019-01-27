edbe5f4064fd9b3fa65456e9de5c2735842594a3
JinLingChristopher <jinl1037@hotmail.com>
2019-01-27 22:23:03 +0800

    complete bomb lab.

--- lab2-bomb/bomb.c
+++ lab2-bomb/bomb.c
@@ -22,8 +22,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "support.h"
 #include "phases.h"
+#include "support.h"
 
 /* 
  * Note to self: Remember to erase this file so my victims will have no
@@ -33,8 +33,7 @@
 
 FILE *infile;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     char *input;
 
     /* Note to self: remember to port this bomb to Windows and put a 
@@ -42,25 +41,25 @@ int main(int argc, char *argv[])
 
     /* When run with no arguments, the bomb reads its input lines 
      * from standard input. */
-    if (argc == 1) {  
-	infile = stdin;
-    } 
+    if (argc == 1) {
+        infile = stdin;
+    }
 
     /* When run with one argument <file>, the bomb reads from <file> 
      * until EOF, and then switches to standard input. Thus, as you 
      * defuse each phase, you can add its defusing string to <file> and
      * avoid having to retype it. */
     else if (argc == 2) {
-	if (!(infile = fopen(argv[1], "r"))) {
-	    printf("%s: Error: Couldn't open %s\n", argv[0], argv[1]);
-	    exit(8);
-	}
+        if (!(infile = fopen(argv[1], "r"))) {
+            printf("%s: Error: Couldn't open %s\n", argv[0], argv[1]);
+            exit(8);
+        }
     }
 
     /* You can't call the bomb with more than 1 command line argument. */
     else {
-	printf("Usage: %s [<input_file>]\n", argv[0]);
-	exit(8);
+        printf("Usage: %s [<input_file>]\n", argv[0]);
+        exit(8);
     }
 
     /* Do all sorts of secret stuff that makes the bomb harder to defuse. */
@@ -70,9 +69,9 @@ int main(int argc, char *argv[])
     printf("which to blow yourself up. Have a nice day!\n");
 
     /* Hmm...  Six phases must be more secure than one phase! */
-    input = read_line();             /* Get input                   */
-    phase_1(input);                  /* Run the phase               */
-    phase_defused();                 /* Drat!  They figured it out!
+    input = read_line(); /* Get input                   */
+    phase_1(input);      /* Run the phase               */
+    phase_defused();     /* Drat!  They figured it out!
 				      * Let me know how they did it. */
     printf("Phase 1 defused. How about the next one?\n");
 
@@ -95,7 +94,7 @@ int main(int argc, char *argv[])
     phase_4(input);
     phase_defused();
     printf("So you got that one.  Try this one.\n");
-    
+
     /* Round and 'round in memory we go, where we stop, the bomb blows! */
     input = read_line();
     phase_5(input);
@@ -110,6 +109,6 @@ int main(int argc, char *argv[])
 
     /* Wow, they got it!  But isn't something... missing?  Perhaps
      * something they overlooked?  Mua ha ha ha ha! */
-    
+
     return 0;
 }
