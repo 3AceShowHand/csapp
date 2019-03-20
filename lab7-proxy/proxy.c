2ff6aaa690d197649033ee4e088910b48ae84937
JinLingChristopher <jinl1037@hotmail.com>
2019-03-21 01:31:42 +0800

    complete proxy

--- lab7-proxy/proxy.c
+++ lab7-proxy/proxy.c
@@ -18,6 +18,122 @@ typedef struct {
 	sem_t items;
 } sbuf_t;
 
+typedef struct {
+	char method[10];
+	char host[50];
+	char port[10];
+	char query[50];
+	char version[10];
+	char user_agent[100];
+	char accept_language[50];
+	char connection[6];
+	char proxy_connection[6];
+} request;
+
+/* cache block*/
+typedef struct {
+	char name[MAXBUF];
+	char data[MAX_OBJECT_SIZE];
+} block_t;
+
+typedef struct {
+	int cursor;
+	block_t buffer[10];
+} cache_t;
+
+void init_cache();
+void cache_update(char* name, int nlen, char* buf, int buflen);
+void sbuf_init(sbuf_t* sp, int n);
+void sbuf_deinit(sbuf_t* sp);
+void sbuf_insert(sbuf_t* sp, int item);
+int sbuf_remove(sbuf_t* sp);
+void build_query(request* req, const char* str);
+void parse_useragent(request* req, char* line);
+void parse_language(request* req, char* line);
+void parse_host_query(request* req, char* line);
+void parse_title(request* req, char* line);
+request* build_request(char* buf, int len);
+void destroy_request(request* req);
+void send_request(int fd, request* req);
+void doit(int fd);
+void *thread(void* vargp);
+
+cache_t cache;
+sem_t cache_mutex;
+sbuf_t sbuf;
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
+	char hostname[MAXBUF], port[MAXBUF];
+	struct sockaddr_storage clientaddr;
+	socklen_t clientlen;
+	int listenfd, connfd;
+	pthread_t tid;
+
+	init_cache();
+	listenfd = Open_listenfd(argv[1]);
+	sbuf_init(&sbuf, SBUFSIZE);
+	clientlen = sizeof(clientaddr);
+	for (int i = 0; i < NTHREADS; i++) {
+		Pthread_create(&tid, NULL, thread, NULL);
+	}
+
+	while (1) {
+		connfd = Accept(listenfd, (SA*)&clientaddr, &clientlen);
+		Getnameinfo((SA*)&clientaddr, clientlen, hostname, MAXBUF,
+				port, MAXBUF, 0);
+		fprintf(stdout, "Accepted connection from (%s, %s)\n", hostname, port);
+		sbuf_insert(&sbuf, connfd);
+	}
+}
+
+void init_cache() {
+	Sem_init(&cache_mutex, 0, 1);
+	cache.cursor = 0;
+	int i;
+	for (i = 0; i < 10; i++) {
+		memset(cache.buffer[i].name, 0, MAXBUF);
+		memset(cache.buffer[i].data, 0, MAX_OBJECT_SIZE);
+	}
+}
+
+/*
+ * find block in cache, by name (host/query)
+ * return -1 on failed, else return the index of block
+*/
+
+int find_cache(char* name, int nlen) {
+	P(&cache_mutex);
+	int i;
+	for (i = 0; i < 10; i++) {
+		if (strncmp(name, cache.buffer[i].name, nlen) == 0) {
+			V(&cache_mutex);
+			return i;
+		}
+	}
+	V(&cache_mutex);
+	return -1;
+}
+
+void cache_update(char* name, int nlen, char* buf, int buflen) {
+	P(&cache_mutex);
+	strncpy(cache.buffer[cache.cursor].name, name, nlen);
+	strncpy(cache.buffer[cache.cursor].data, buf, buflen);
+	cache.cursor = (cache.cursor + 1) % 10;
+	V(&cache_mutex);
+}
+
+void send_cache_block(int idx, int fd) {
+	P(&cache_mutex);
+	char buf[MAX_OBJECT_SIZE];
+	strcpy(buf, cache.buffer[idx].data);
+	Rio_writen(fd, buf, strlen(buf));
+	V(&cache_mutex);
+}
+
 void sbuf_init(sbuf_t* sp, int n) {
 	sp->buf = Calloc(n, sizeof(int));
 	sp->n = n;
@@ -48,18 +164,6 @@ int sbuf_remove(sbuf_t* sp) {
 
 	return item;
 }
-
-typedef struct {
-	char method[10];
-	char host[50];
-	char port[10];
-	char query[50];
-	char version[10];
-	char user_agent[100];
-	char accept_language[50];
-	char connection[6];
-	char proxy_connection[6];
-} request;
 
 void build_query(request* req, const char* str) {
 	memset(req->query, 0, strlen(req->query));
@@ -139,24 +243,41 @@ void destroy_request(request* req) {
  * each field obtained by parse functions above.
  * each function parse lines by call strtok_r function to separte each words in the line.
 */
-
 void send_request(int fd, request* req) {
-	char buf[MAXBUF];
-	sprintf(buf, "%s %s %s\r\n", req->method, req->query, req->version);
-	sprintf(buf, "%sHost: %s\r\n", buf, req->host);
-	sprintf(buf, "%sConnection: %s\r\n", buf, req->connection);
-	sprintf(buf, "%sProxy Connetion: %s\r\n\r\n", buf, req->proxy_connection);
+	char name[MAXBUF];
+	sprintf(name, "%s/%s", req->host, req->query);
+	int idx = find_cache(name, strlen(name));
+	if (idx != -1) {
+		fprintf(stdout, "find %s in cache\n", name);
+		send_cache_block(idx, fd);
+	} else {
+		fprintf(stdout, "cannot find %s in cache, fetch from server\n", name);
+		char buf[MAXBUF];
+		sprintf(buf, "%s %s %s\r\n", req->method, req->query, req->version);
+		sprintf(buf, "%sHost: %s\r\n", buf, req->host);
+		sprintf(buf, "%sConnection: %s\r\n", buf, req->connection);
+		sprintf(buf, "%sProxy Connetion: %s\r\n\r\n", buf, req->proxy_connection);
 
-	int proxyfd = Open_clientfd(req->host, req->port);
-	rio_t rio;
-	Rio_readinitb(&rio, proxyfd);
-	Rio_writen(proxyfd, buf, strlen(buf));
-	int n;
-	memset(buf, 0, MAXBUF);
-	while ((n = rio_readlineb(&rio, buf, MAXBUF)) != 0) {
-		Rio_writen(fd, buf, n);
+		int proxyfd = Open_clientfd(req->host, req->port);
+		rio_t rio;
+		Rio_readinitb(&rio, proxyfd);
+		Rio_writen(proxyfd, buf, strlen(buf));
+		int n;
+		memset(buf, 0, MAXBUF);
+		int total = 0;
+		char block[MAX_OBJECT_SIZE];
+		while ((n = rio_readlineb(&rio, buf, MAXBUF)) != 0) {
+			Rio_writen(fd, buf, n);
+			if (total < MAX_OBJECT_SIZE) {
+				strncpy(block+total, buf, n);
+			}
+			total += n;
+		}
+		if (total < MAX_OBJECT_SIZE) {
+			cache_update(name, strlen(name), block, strlen(block));
+		}
+		Close(proxyfd);
 	}
-	Close(proxyfd);
 	Close(fd);
 }
 
@@ -179,84 +300,10 @@ void doit(int fd) {
 	destroy_request(req);
 }
 
-//void* thread(void* vargp) {
-//	int connfd = *((int*)vargp);
-//	Pthread_detach(Pthread_self());
-//	Free(vargp);
-//	doit(connfd);
-//
-//	return NULL;
-//}
-
-sbuf_t sbuf;
-
 void *thread(void* vargp) {
 	Pthread_detach(Pthread_self());
 	while (1) {
 		int connfd = sbuf_remove(&sbuf);
 		doit(connfd);
 	}
-	return NULL;
 }
-
-int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, "usage: %s <port>\n", argv[0]);
-		exit(1);
-	}
-	char hostname[MAXBUF], port[MAXBUF];
-	struct sockaddr_storage clientaddr;
-	socklen_t clientlen;
-	int listenfd, connfd;
-	pthread_t tid;
-
-	listenfd = Open_listenfd(argv[1]);
-	sbuf_init(&sbuf, SBUFSIZE);
-	clientlen = sizeof(clientaddr);
-	for (int i = 0; i < NTHREADS; i++) {
-		Pthread_create(&tid, NULL, thread, NULL);
-	}
-
-	while (1) {
-		connfd = Accept(listenfd, (SA*)&clientaddr, &clientlen);
-		Getnameinfo((SA*)&clientaddr, clientlen, hostname, MAXBUF,
-				port, MAXBUF, 0);
-		fprintf(stdout, "Accepted connection from (%s, %s)\n", hostname, port);
-		sbuf_insert(&sbuf, connfd);
-	}
-
-//	while (1) {
-//
-//		connfdp = Malloc(sizeof(int));
-//		*connfdp = Accept(listenfd, (SA*)&clientaddr, &clientlen);
-//		Getnameinfo((SA*)&clientaddr, clientlen, hostname, MAXBUF,
-//		            port, MAXBUF, 0);
-//		fprintf(stdout, "Accepted connection from (%s, %s)\n", hostname, port);
-//		Pthread_create(&tid, NULL, thread, connfdp);
-//	}
-}
-
-
-//int main(int argc, char* argv[]) {
-//	if (argc != 2) {
-//		fprintf(stderr, "usage: %s <port>\n", argv[0]);
-//		exit(1);
-//	}
-//	char hostname[MAXBUF], port[MAXBUF];
-//	struct sockaddr_storage clientaddr;
-//	socklen_t clientlen;
-//	int listenfd, *connfdp;
-//	pthread_t tid;
-//
-//	listenfd = Open_listenfd(argv[1]);
-//
-//	while (1) {
-//		clientlen = sizeof(clientaddr);
-//		connfdp = Malloc(sizeof(int));
-//		*connfdp = Accept(listenfd, (SA*)&clientaddr, &clientlen);
-//		Getnameinfo((SA*)&clientaddr, clientlen, hostname, MAXBUF,
-//		            port, MAXBUF, 0);
-//		fprintf(stdout, "Accepted connection from (%s, %s)\n", hostname, port);
-//		Pthread_create(&tid, NULL, thread, connfdp);
-//	}
-//}
